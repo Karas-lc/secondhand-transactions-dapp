a3a375123d7f3631424614df819f7b3b0c2f6838
Karas-lc <82162079+Karas-lc@users.noreply.github.com>
2022-08-25 17:18:22 +0800

    1 minlestone

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
+import PublishPage from "./pages/PublishPage";
 
 function App() {
   return <div>
     <Routes>
       <Route path="/details" element={<DetailPage />} />
+      <Route path="/publish" element={<PublishPage /> } />
       <Route exact path="/" element={<HomePage />} />
     </Routes>
   </div>
--- src/components/NavivationBar.jsx
+++ src/components/NavivationBar.jsx
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import Button from "@mui/material/Button";
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -56,23 +57,7 @@ function NavigationBar() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                    >
-                        MUI
-                    </Typography>
+                    <Button variant="contained" href="/publish">Publish</Button>
                     <Search>
                         <SearchIconWrapper>
                             <SearchIcon />
