cc4601be64f95a592401503279d5e7a33ffe6ca5
djy6969 <jingyue.dou@ucdconnect.ie>
2022-08-26 22:44:58 +0800

    ipfs

--- src/pages/PublishPage.jsx
+++ src/pages/PublishPage.jsx
@@ -4,49 +4,105 @@ import Box from "@mui/material/Box";
 import {Button, message, Upload} from "antd";
 import Web3 from "web3";
 import {LoadingOutlined, PlusOutlined} from "@ant-design/icons";
-import { useWeb3 } from '@openzeppelin/network/react';
 import { create, CID, IPFSHTTPClient } from "ipfs-http-client";
-
+import {useWeb3} from "@openzeppelin/network/lib/react";
+var react_1 = require("react");
+// var ipfs_http_client_1 = require("ipfs-http-client");
 const infuraProjectId = '2Dqfuw4lxAJbkviO7vv2pGAtaSn';
 const infuraProjectSecret = '96e52de36d0828f5b257be9b9c46985c';
 const authorization = "Basic " + btoa(infuraProjectId + ":" + infuraProjectSecret);
 function publishPage() {
-    const [images, setImages] = React.useState<{ cid: CID, path: string }[]>([]);
-
+    // const [images, setImages] = React.useState<{ cid: CID;path: string }[]>([]);
+    // const imageData = React.useState([]);
+    // const images = imageData[0];
+    // const setImages = imageData[1];
+    //
+    // let ipfs: IPFSHTTPClient | undefined;
+    // try {
+    //     ipfs = create({
+    //         url: "https://ipfs.infura.io:5001/api/v0",
+    //         headers: {
+    //             authorization,
+    //         },
+    //     });
+    // } catch (error) {
+    //     console.error("IPFS error ", error);
+    //     ipfs = undefined;
+    // }
+    // const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    //     event.preventDefault();
+    //     const form = event.target as HTMLFormElement;
+    //     const files = (form[0] as HTMLInputElement).files;
+    //
+    //     if (!files || files.length === 0) {
+    //         return alert("No files selected");
+    //     }
+    //
+    //     const file = files[0];
+    //     // upload Files
+    //     const result = await (ipfs as IPFSHTTPClient).add(file);
+    //
+    //
+    //     // @ts-ignore
+    //     setImages(uniqueImages);
+    //
+    //     form.reset();
+    // };
+    //
+    // console.log("images ", images);
+    var _this = this;
+    var _a = React.useState([]), images = _a[0], setImages = _a[1];
     let ipfs: IPFSHTTPClient | undefined;
     try {
         ipfs = create({
             url: "https://ipfs.infura.io:5001/api/v0",
             headers: {
-                authorization,
-            },
+                authorization: authorization
+            }
         });
-    } catch (error) {
+    }
+    catch (error) {
         console.error("IPFS error ", error);
         ipfs = undefined;
     }
-    const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const form = event.target as HTMLFormElement;
-        const files = (form[0] as HTMLInputElement).files;
-
-        if (!files || files.length === 0) {
-            return alert("No files selected");
-        }
-
-        const file = files[0];
-        // upload Files
-        const result = await (ipfs as IPFSHTTPClient).add(file);
-
-
-        // @ts-ignore
-        setImages(uniqueImages);
-
-        form.reset();
-    };
-
+    /**
+     * @description event handler that uploads the file selected by the user
+     */
+    var onSubmitHandler = function (event) { return __awaiter(_this, void 0, void 0, function () {
+        var form, files, file, result, uniquePaths, uniqueImages;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    event.preventDefault();
+                    form = event.target;
+                    files = form[0].files;
+                    if (!files || files.length === 0) {
+                        return [2 /*return*/, alert("No files selected")];
+                    }
+                    file = files[0];
+                    return [4 /*yield*/, ipfs.add(file)];
+                case 1:
+                    result = _a.sent();
+                    uniquePaths = new Set(__spreadArray(__spreadArray([], images.map(function (image) { return image.path; }), true), [
+                        result.path,
+                    ], false));
+                    uniqueImages = __spreadArray([], uniquePaths.values(), true).map(function (path) {
+                        return __spreadArray(__spreadArray([], images, true), [
+                            {
+                                cid: result.cid,
+                                path: result.path
+                            },
+                        ], false).find(function (image) { return image.path === path; });
+                    });
+                    // @ts-ignore
+                    setImages(uniqueImages);
+                    form.reset();
+                    return [2 /*return*/];
+            }
+        });
+    }); };
     console.log("images ", images);
-    const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+    const web3Context = useWeb3(`wss://goerli.infura.io/ws/v3/${infuraProjectId}`);
     const { networkId, networkName, providerName } = web3Context;
     const [loading, setLoading] = useState(false);
     const [account, setAccount] = useState();
